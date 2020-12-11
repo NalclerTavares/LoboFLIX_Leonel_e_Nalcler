05a4da6484922b9c243b2429998d8a1743794de3
NalclerTavares <61871779+NalclerTavares@users.noreply.github.com>
2020-12-11 00:43:59 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import dadosIniciais from './data/dados.json'
+import Banner from './components/Banner'
+import Carousel from "./components/Carousel"
+function App() {
+  return (
+    <div>
+     
+     <Banner 
+       videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
+       url={dadosIniciais.categorias[0].videos[0].url} 
+       videoDescription={"Projeto InclusaoFLIX"} /> 
+      
+     <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+        />
+        <Carousel
+        category={dadosIniciais.categorias[1]}
+        />
+        <Carousel
+        category={dadosIniciais.categorias[2]}
+        />
+        <Carousel
+        category={dadosIniciais.categorias[3]}
+     />
+
+   </div> 
+); 
+} 
+export default App; 
